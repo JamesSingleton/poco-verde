be49c776b9860683cafb82eae0ea30d25c629c3d
James Singleton <jamesrsingleton1@gmail.com>
2021-07-02 09:24:41 -0700

    chore(images): switch from img to Image

--- pages/index.js
+++ pages/index.js
@@ -25,38 +25,48 @@ const Index = () => (
         </p>
         <div className="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <img
+            <Image
               className="h-12"
               src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
               alt="Tuple"
+              height="48"
+              width="105"
             />
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <img
+            <Image
               className="h-12"
               src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg"
               alt="Mirage"
+              height="48"
+              width="105"
             />
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <img
+            <Image
               className="h-12"
               src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
               alt="StaticKit"
+              height="48"
+              width="105"
             />
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 md:col-start-2 lg:col-span-1">
-            <img
+            <Image
               className="h-12"
               src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
               alt="Transistor"
+              height="48"
+              width="105"
             />
           </div>
           <div className="col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1">
-            <img
+            <Image
               className="h-12"
               src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
               alt="Workcation"
+              height="48"
+              width="105"
             />
           </div>
         </div>
